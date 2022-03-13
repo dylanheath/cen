2b96c7ba89ed9c28f1b62213d6a77211e0fa827a
Dylan <dylanheathcote2004@gmail.com>
2022-03-13 12:04:47 +0000

    token type
    Signed-off-by: Dylan <dylanheathcote2004@gmail.com>

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,6 +1,13 @@
+type token = {
+  name: string,
+  image: string,
+  contract: string | null,
+}
+
 export const tokens = {
 	XTZ: {
 	  name: "XTZ",
 	  image: "not found",
+	  contract: "not found",
 	}
 }
