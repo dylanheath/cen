adf258e9e6948d52c3abbf6c1b6236e77b74119f
dylan heathcote <dylanheathcote@dylans-mbp.home>
2022-03-13 18:13:08 +0000

    USDTZ to token list
    Signed-off-by: dylan heathcote <dylanheathcote@dylans-mbp.home>

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -3,13 +3,23 @@ type token = {
     name: string,
     image: string,
     contract: string | null,
-  }
+   }
+   USDTZ: {
+     name: string,
+     image: string,
+     contract: string | null,
+   }
 }
 
 export const tokens: token = {
   XTZ: {
     name: "XTZ",
     image: "not found",
+    contract: "not found",
+  }
+  USDTZ: {
+    name: "USDTZ",
+    image: "not found", 
     contract: "not found",
   }
 }
