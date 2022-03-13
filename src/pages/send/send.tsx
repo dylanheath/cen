82a92128b7c9515a3afb524fa133000c970779ab
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-13 22:09:19 +0000

    useEffect send request
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/pages/send/send.tsx
+++ src/pages/send/send.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 // Context
@@ -14,12 +15,53 @@ import './send.css';
 import { tokens } from '../../utils/tokens';
 import { getActiveAccount, sendXTZ, sendUSDtz } from '../../utils/wallet';
 
-export default function send() {
+// assets
+import DefaultIcon from '../../assets/default.png';
+
+export default function Send() {
+  const { User, setUser } = useContext<any>(UserContext);
+  const [Reciever, setReceiver]  = useState<string | null>(null);
+  const [Amount, setAmount] = useState<number>(0);
+  const [Message, setMessage] = useState<string | null>(null);
+  const [Contacts, setContacts] = useState<Array<string | null>>(['']);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (User.status == true) {
+        const address = await User.address?.toString();
+	const UserContacts = await User?.contacts;
+	if (address | UserContacts) {
+	  const getContacts = await axios.get(`${api.url}/user/contacts/list/users${address}`, { params: { contactslist: UserContacts } }) 
+	  .then((response) => {
+            const ContactsData = response.data;
+	    setContacts(ContactsData);
+	  })
+	  .catch(() => {
+	    console.log('could not grab contacts')
+	  })
+	}  
+      }
+    }
+    fetchContacts();
+  }, [])
   return  (
     <div className="send">
+      <div className="send-contact-popup-container">
+      </div>
       <div className="send-container">
         <div className="send-box">
 	  <div className="send-header">
+	  </div>
+	  <div className="send-input-container">
+            <input className="send-amount-input" />
+	  </div>
+	  <div className="send-contact-button-container">
+            <button className="send-contact-button" type="button">
+	      <div className="send-account-container">
+	        <img className="send-accout-picture" src={DefaultIcon} />
+                <p className="send-account-name"></p>
+	      </div>
+	    </button>
 	  </div>
 	</div>
       </div>
