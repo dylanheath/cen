687470a03995032132aa95d4fa076add13ac38bb
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-19 21:44:03 +0000

    added scroll for tokens & added tokens assets to object
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/dashboard/Token.tsx
+++ src/components/dashboard/Token.tsx
@@ -8,12 +8,17 @@ export default function Token({TokensList} : {TokensList:Array<string>}) {
   return (
     <div className="Token-template-container">
       {TokensList.map((tok:any) => (
-        <div className="token-template">
-          <div className="token-information-container">
-	    <p className="token-name">{tok.symbol}</p>
-	    <p className="token-balance">{tok.balance}</p>
-          </div>
-        </div>
+        <div>
+	  <div className="token-container">
+            <div className="token-template">
+              <div className="token-information-container">
+	        <p className="token-name">{tok.symbol}</p>
+	        <p className="token-balance">{tok.balance}</p>
+              </div>
+            </div>
+	  </div>
+	  <hr className="token-divider"></hr>
+	</div>
       ))} 
     </div>
   )
--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,18 +1,37 @@
 import React from 'react';
+
+//assets
 import TezosIcon from '../assets/TezosCoin.png';
+import USDtzIcon from '../assets/USDtez.png';
+import tzBTCIcon from '../assets/tzBTC.svg';
 
 type token = {
-   XTZ: {
+   "XTZ": {
     name: string,
     image: string,
-    contract: string | null,
    },
+   "USDtz": {
+    name: string,
+    image: string,
+   },
+   "tzBTC": {
+    name: string,
+    image: string,
+   }
+
 }
 
 export const tokens: token = {
-  XTZ: {
+  "XTZ": {
     name: "XTZ",
     image: TezosIcon,
-    contract: "not found",
+  },
+  "USDtz": {
+    name: "USDtz",
+    image: USDtzIcon,
+  },
+  "tzBTC": {
+    name: "tzBTC",
+    image: tzBTCIcon,
   }
 }
