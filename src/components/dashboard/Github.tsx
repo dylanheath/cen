8adf4db7453b982a71fa5429126f28a8ee00794a
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-21 20:45:52 +0000

    github box
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/dashboard/Github.tsx
+++ src/components/dashboard/Github.tsx
@@ -10,23 +10,15 @@ export default function Github() {
 
   return (
     <div className="Github">
-      <div className="Github-container">
-        <div className="Github-box">
-          <div className="Github-header-container">
-            <p className="Github-header">Github</p> 
-	    <img className="Github-header-icon" src={GithubIcon} />
-	  </div>
-	  <div className="Github-content-container">
-	    <div className="Github-content">
-	    </div>
-	    <div className="Github-repo-container">
-              <div className="Github-cen">
-	      </div>
-	      <div className="Github-cen-api">
-	      </div>
-	    </div>
-	  </div>
-	</div>
+      <div className="Github-header-container">
+        <p className="Github-header">Github</p> 
+      <img className="Github-header-icon" src={GithubIcon} />
+      </div>
+      <div className="Github-content-container">
+        <p className="Github-content">View Source code and Contribute</p>
+      </div>
+      <div className="Github-button-container">
+        <button className="Github-button" type="button">View Repo</button>
       </div>
     </div>
   )
