f209063c9089adb3cb518a692d8ed642ab42364f
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-28 23:44:30 +0100

    liquidity data
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/liquidity/liquidity.tsx
+++ src/components/liquidity/liquidity.tsx
@@ -17,12 +17,15 @@ import CENicon from '../../assets/CEN.png';
 export default function LiquidityBox() {
   const { User, setUser } = useContext<any>(UserContext);
   const [CenOutput, setCenOutput] = useState<number>(0);
+  const [PoolShare, setPoolShare] = useState<number>(0);
+  const [CenPerXTZ, setCenPerXTZ] = useState<number>(0);
+  const [XtzPerCEN, setXtzPerCEN] = useState<number>(0);
   return (
     <div>
     <div className="Liquidity-box">
       <div className="liquidity-header-container">
         <p className="liquidity-header">Liquidity</p> 
-	<button className="liquidity-header-button" type="button">Settings</button>
+	<button className="liquidity-header-button" type="button">More</button>
       </div>
       <div className="liquidity-input-container">
         <div className="liquidity-amount-input-box">
@@ -58,6 +61,22 @@ export default function LiquidityBox() {
       </div>
       <div className="liquidity-add-container">
         <button className="liquidity-add" type="button">Add</button>
+      </div>
+    </div>
+    <div className="liquidity-data-container">
+      <div className="liquidity-data-box">
+        <div className="Cen-data">
+          <p className="liquidity-data-header">CEN per XTZ</p>
+	  <p className="liquidity-data">{CenPerXTZ}</p>
+        </div>
+        <div className="XTZ-data">
+          <p className="liquidity-data-header">XTZ per CEN</p>
+	  <p className="liquidity-data">{XtzPerCEN}</p>
+        </div>
+        <div className="Pool-share">
+          <p className="liquidity-data-header">Pool Share</p>
+	  <p className="liquidity-data">{PoolShare}</p>
+        </div>
       </div>
     </div>
     </div>
