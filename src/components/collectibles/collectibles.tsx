554743528eebf478eca1b9516eef74ab46f0ddc5
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-29 15:39:07 +0100

    collectibles components
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

---
+++ src/components/collectibles/collectibles.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+
+// styling
+import './collectibles.css'
+
+export default function Collectibles() {
+
+  return (
+    <div className="Collectibles">
+      <div className="collectibles-container">
+      </div>
+    </div>
+  )
+}
+
---
+++ src/pages/collectibles/collectibles.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+
+export default function Collectibles() {
+
+  return (
+    <div className="Collectibles">
+      <div className="collectibles-container">
+
+      </div>
+    </div>
+  )
+}
+
