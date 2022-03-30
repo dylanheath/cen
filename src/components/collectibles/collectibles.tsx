0131a68a48037b44ad2ae7fcc21e976dfe8b5484
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-30 14:11:29 +0100

    nft viewer + load animations
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/collectibles/collectibles.tsx
+++ src/components/collectibles/collectibles.tsx
@@ -10,6 +10,15 @@ import { api } from '../../utils/api';
 // styling
 import './collectibles.css'
 
+interface NFTfields {
+  name: string,
+  description: string,
+  creator: string,
+  alias: any,
+  display: string,
+  collected: number,
+}
+
 export default function CollectiblesBox() {
   const { User, setUser } = useContext<any>(UserContext);
   const [NFTS, setNFTS] = useState<Array<string | null>>([null]);
@@ -17,14 +26,22 @@ export default function CollectiblesBox() {
     let isMounted = true;
     const fetchNFTS = async () => {
       const address = await User.address?.toString();
-      const NFTcollection: Array<string> = [];
+      const NFTcollection: Array<any> = [];
       if (User.status === true) {
         const getNFTs = await axios.get(`https://api.better-call.dev/v1/account/mainnet/${address}/token_balances`, { timeout: 4000 })
 	  .then((response) => {
             const NFTdata = response.data.balances;
 	    NFTdata.map((nft:any) => {
 	      if (nft.token_id > 0 &&  nft.decimals === 0) {
-                NFTcollection.push(nft); 
+	        const NFTobj: NFTfields = {
+                  name: nft.name, 
+		  description: nft.description,
+                  creator: nft.creators,
+		  alias: null, 
+		  display: nft.display_uri.replace("ipfs://", ""),
+		  collected: nft.balance,
+	        }
+                NFTcollection.push(NFTobj); 
 	      }
 	    })
 	    console.log(NFTdata);
@@ -42,10 +59,15 @@ export default function CollectiblesBox() {
     <div className="Collectibles-container">
         <div className="Collectibles">
           {NFTS.map((nftData:any) => (
-            <div className="collectible-template">
-	      <img className="nft-image" src={`https://ipfs.io/ipfs/${nftData?.display_uri.replace("ipfs://", "")}`} /> 
-	      <p className="nft-name">{nftData?.name}</p>
-	    </div>
+            <button className="collectible-template">
+	      <img className="nft-image" src={`https://ipfs.io/ipfs/${nftData?.display}`} /> 
+	      <div className="nft-information-container">
+	        <div className="nft-name-collected-container">
+	          <p className="nft-name">{nftData?.name.length > 19 ? nftData?.name.slice(0, - 3 ) + " ..." : nftData?.name}</p>
+		  <p className="nft-collected">{nftData?.collected == 0 ? "1" : nftData?.collected} x</p>
+		</div>
+	      </div>
+	    </button>
           ))}
         </div>
     </div>
