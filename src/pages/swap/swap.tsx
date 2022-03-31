ab51dab025cd57af41c2070bc74474f3458687da
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-03-31 11:38:51 +0100

    swap styling and component
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/pages/swap/swap.tsx
+++ src/pages/swap/swap.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// utils
+import { tokens } from '../../utils/tokens';
+
+// assets
+import ArrowDown from '../../assets/arrowdown.png';
+import ArrowIn from '../../assets/In.png';
+import CenIcon from '../../assets/CEN.png';
+
+// styling
+import './swap.css';
 
 export default function Swap() {
-  const hello: string = "hello";
+  const [SwapAmount, setSwapAmount] = useState<number>(0);
   return (
-    <div className="swap">
-      <p>swap</p>
+    <div className="Swap">
+      <div className="swap-container">
+        <div className="swap-box">
+	  <div className="swap-header-container">
+	    <p className="swap-header">Swap</p>
+	    <button className="swap-header-button">Settings</button>
+	  </div>
+	  <div className="swap-input-container">
+	    <div className="swap-amount-input-box">
+              <input className="swap-amount-input" placeholder="0.0" type="text" pattern="^[0-9]*[.,]?[0-9]*$" inputMode="decimal" defaultValue="0.00"/>
+	      <div className="swap-token-select-container">
+                <button className="swap-token-select-button">
+		  <div className="swap-token-select-content-container">
+		    <img className="swap-token-select-icon" src={tokens.XTZ.image} />
+		    <p className="swap-token-select-name">XTZ</p>
+		    <img className="swap-token-arrow-down" src={ArrowDown} />
+		  </div>
+		</button>
+	      </div>
+	    </div>
+	  </div>
+	  <div className="swap-arrow-down-container">
+	    <img className="swap-arrow-down" src={ArrowIn} />
+	  </div>
+	  <div className="swap-output-container">
+	    <div className="swap-output-button">
+              <div className="swap-amount-container">
+                <p className="swap-amount">{SwapAmount}</p>
+		<div className="swap-token-select-container">
+                  <button className="swap-token-select-button">
+                    <div className="swap-token-select-content-container">
+                      <img className="swap-token-select-icon" src={CenIcon} /> 
+		      <p className="swap-token-select-name">CEN</p>
+		      <img className="swap-token-arrow-down" src={ArrowDown} />
+		    </div>
+		  </button>
+		</div>
+	      </div>
+	    </div>
+	  </div>
+	  <div className="swap-button-container">
+	    <button className="swap-button">Swap</button>
+	  </div>
+	</div>
+      </div>
     </div>
   )
 }
