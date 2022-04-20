77ab8b72a879ab20dff7fa938d55613aec81c903
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-04-20 11:22:23 +0100

    wallet conenct function
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/connect/connectBox.tsx
+++ src/components/connect/connectBox.tsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState, useContext} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 // context
 import { UserContext } from '../../context/context';
@@ -10,11 +12,54 @@ import CenLogo from '../../assets/cen/CenWhite.png'
 import './connect.css';
 
 // utils
-import { connectWallet, getActiveAccount } from '../../utils/wallet';
+import { api } from '../../utils/api';
+
+// utils
+import { connectWallet, disconnectWallet, getActiveAccount, checkIfWalletConnected } from '../../utils/wallet';
 
 export default function ConnectBox() {
   const { User, setUser } = useContext<any>(UserContext);
+  const navigate = useNavigate();
+  const [ConnectionState, setConnectionState] = useState<boolean>(false);
   const [BetaAccept, setBetaAccept] = useState<boolean>(false);
+
+  const ConnectWallet = async () => {
+    const activeAccount = await getActiveAccount();
+    let myAddress: String | undefined;
+    if (!activeAccount) {
+      const getAddress = await connectWallet();
+      // @ts-ignore
+      myAddress = getAddress;
+      console.log('New connection: ', myAddress);
+      // eslint-disable-next-line no-unused-vars
+      myAddress = getAddress.toString();
+      setConnectionState(true);
+      // eslint-disable-next-line no-unused-vars
+      await axios.get(`${api.url}/user/${myAddress}`)
+        .then((response) => {
+          const UserData = response.data;
+          const userdata = {
+            name: UserData.User,
+            address: myAddress,
+            cid: UserData.CID,
+            email: UserData.Email,
+            contact: UserData.Contacts,
+	    token: UserData.Token,
+	    status: true,
+          };
+          setUser(userdata);
+	  // setPfp(`${ipfs.url}${UserData.CID}`);
+          console.log(UserData);
+          console.log('data has been received');
+        })
+        .catch(() => {
+          console.log('error grabbing user');
+          navigate('/app/signup');
+        });
+      navigate('/app/dashboard');
+    }
+  };
+
   return (
     <div className="Connect-container">
       <div className="Connect-box">
