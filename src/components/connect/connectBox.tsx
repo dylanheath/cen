aa8cf59ec4ce3ec8e93a387f10d750896950768d
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-04-20 11:03:07 +0100

    wallet utils
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/connect/connectBox.tsx
+++ src/components/connect/connectBox.tsx
@@ -9,15 +9,18 @@ import CenLogo from '../../assets/cen/CenWhite.png'
 // styling
 import './connect.css';
 
+// utils
+import { connectWallet, getActiveAccount } from '../../utils/wallet';
+
 export default function ConnectBox() {
   const { User, setUser } = useContext<any>(UserContext);
   const [BetaAccept, setBetaAccept] = useState<boolean>(false);
   return (
     <div className="Connect-container">
       <div className="Connect-box">
-        <div className="connect-header-container">
-	  <p className="connect-header">Connect Wallet</p>
-        </div>
+        <div className="connect-accept-container">
+
+	</div>
 	<hr className="connect-divider"></hr>
         <div className="connect-content-container">
         </div>
