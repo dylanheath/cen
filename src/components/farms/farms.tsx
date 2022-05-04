4c2736f61d8f4cd98504060fe05c9ab75f92766f
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-05-04 10:20:14 +0100

    toFixed usd and xtz
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/farms/farms.tsx
+++ src/components/farms/farms.tsx
@@ -91,10 +91,10 @@ export default function Farms() {
           <p className="farms-analytics-header">Total Staking</p>
 	</div>
 	<div className="farms-analytics-amount-container">
-	  <p className="farms-available-rewards">{XTZprice * TotalStakingUSD == 0 ? "0.00" : XTZprice * TotalStakingUSD}</p> 
+	  <p className="farms-available-rewards">{XTZprice * TotalStakingUSD == 0 ? "0.00" : XTZprice * Number(TotalStakingUSD.toFixed(2))}</p> 
 	  <p className="farms-available-rewards-xtz">XTZ</p>
 	</div>
-	<p className="farms-available-rewards-usd">{TotalStakingUSD == 0 ? "0.00" : TotalStakingUSD}</p>
+	<p className="farms-available-rewards-usd">{TotalStakingUSD == 0 ? "0.00" : TotalStakingUSD.toFixed(2)}</p>
       </div>
       <div className="farm-boxes-container">
       <div className="farms-box-container">
