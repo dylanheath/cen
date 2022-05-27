b9c6190baf935b54fe06f6108e47171a4c2c7c98
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-05-27 20:54:08 +0100

    no tokens placeholder
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/dashboard/Tokens.tsx
+++ src/components/dashboard/Tokens.tsx
@@ -13,6 +13,7 @@ import './dashboard.css';
 
 // components
 import Token from './Token';
+import {DotWave, LeapFrog, MrMiyagi, Orbit, Pinwheel} from '@uiball/loaders';
 
 interface Tokenfields {
   balance: string,
@@ -24,6 +25,7 @@ export default function Tokens() {
   const { User, setUser } = useContext<any>(UserContext);
   const [UserTokens, setUserTokens] = useState<Array<string>>([]);
   const [TokensReceived, setTokensReceived] = useState<boolean>(false);
+  const [NoTokens, setNoTokens] = useState<boolean>(false);
   const navigate = useNavigate();
   const NFTnav = () => {
     navigate('/app/assets');
@@ -42,8 +44,11 @@ export default function Tokens() {
 	      TokenData.map((tok:any) => {
 	        if ( tok.token.hasOwnProperty('metadata')&& tok.balance !== "0") {
                   TokenBalance.push(tok);
-		  }
+		  } 
 	      })
+	      if (TokenData.length === 0) {
+	        setNoTokens(true);
+	      }
 	      setUserTokens(TokenBalance);
 	      setTokensReceived(true);
 	    })
@@ -64,7 +69,13 @@ export default function Tokens() {
 	    <button className="Token-header-button" onClick={NFTnav}>View More</button>
 	  </div>
 	  <div className="Token-content-container">
-	  {TokensReceived == true && (
+	  {NoTokens == true && (
+            <div className="Token-none-container">
+              <p className="Token-none-tag">No Tokens Owned</p>
+	      <Pinwheel size={40} speed={1.2} color="rgb(33, 114, 229)" />
+	    </div>
+          )}
+	  {TokensReceived == true && NoTokens == false && (
               <Token TokensList={UserTokens} />
 	    )}
 	  </div>
